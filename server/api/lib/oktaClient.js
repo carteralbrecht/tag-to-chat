affbb5955819e726a5bfc02e3067291486a4048b
Ryan DelVecchio <ryan.coastek@gmail.com>
2020-11-04 00:22:20 -0500

    oops

--- server/api/lib/oktaClient.js
+++ server/api/lib/oktaClient.js
@@ -2,7 +2,7 @@ const okta = require('@okta/okta-sdk-nodejs');
 
 const client = new okta.Client({
   orgUrl: 'https://dev-1701617.okta.com',
-  token: '005IViMyo7AJfVKije0NoyK8kp6-n5i-cCaq8Wwzuq'
+  token: process.env.oktaToken
 });
 
 module.exports = client;

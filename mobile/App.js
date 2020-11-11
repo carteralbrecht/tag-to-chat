1ddee53f3ae888e40cd9f9bd9aeb1638bb41fabf
cjack0416 <70340825+cjack0416@users.noreply.github.com>
2020-11-11 12:51:27 -0500

    adding mobile loggin screen

---
+++ mobile/App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import LoginScreen from './client/src/screens/LoginScreen';
+
+export default function App() {
+  return (
+    <LoginScreen />
+  );
+}
---
+++ mobile/client/src/screens/LoginScreen.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import {StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
+
+class LoginScreen extends React.Component {
+
+    state={
+        email: "",
+        password: ""
+    }
+    
+    render(){
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcomeText}>Welcome!{"\n"}Let's Chat</Text>
+                <View style={styles.inputView}>
+                    <TextInput
+                    style={styles.inputText}
+                    placeholder="Email..."
+                    placeholderTextColor="white"
+                    onChangeText={text => this.setState({email:text})}/>
+                </View>
+                <View style={styles.inputView}>
+                    <TextInput
+                    style={styles.inputText}
+                    secureTextEntry
+                    placeholder="Password..."
+                    placeholderTextColor="white"
+                    onChangeText={text => this.setState({password:text})}/>
+                </View>
+                <TouchableOpacity onPress={() => alert("Forgot Password?")}>
+                    <Text style={styles.forgot}>Forgot Password?</Text>
+                </TouchableOpacity> 
+                <TouchableOpacity style={styles.loginBtn}
+                onPress={() => alert("email: " + this.state.email + 
+                "\npassword: " + this.state.password)}>
+                    <Text style={styles.loginText}>LOGIN</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => alert("Signup")}>
+                    <Text style={styles.loginText}>Signup</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#303030",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+
+    welcomeText: {
+        color: "white",
+        fontSize: 30,
+        marginBottom: 40
+    },
+
+    inputView: {
+        width: "80%",
+        backgroundColor: "grey",
+        borderRadius: 25,
+        height: 50,
+        marginBottom: 20,
+        justifyContent: "center",
+        padding: 20
+    },
+
+    inputText: {
+        color: "white",
+        height: 50
+    },
+
+    forgot: {
+        color: "white",
+        fontSize: 11
+    },
+
+    loginText: {
+        color: "white",
+        fontSize: 14
+    },
+
+    loginBtn: {
+        width: "80%",
+        backgroundColor: "#5102A1",
+        borderRadius: 25,
+        height: 50,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 40,
+        marginBottom: 10
+    }
+})
+
+export default LoginScreen;

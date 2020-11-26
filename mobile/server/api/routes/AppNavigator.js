d1982c0cb12220d4476e69605716542a4f1b9e81
cjack0416 <70340825+cjack0416@users.noreply.github.com>
2020-11-26 02:25:19 -0500

    Chat members drawer

--- mobile/server/api/routes/AppNavigator.js
+++ mobile/server/api/routes/AppNavigator.js
@@ -3,15 +3,14 @@ import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Login from "../../../client/src/screens/LoginScreen";
-import Chat from "../../../client/src/screens/MessageScreen";
 import Register from "../../../client/src/screens/Registration";
+import DrawerNavigator from "./ChatDrawerNavigator";
 
 const Stack = createStackNavigator();
 
 export const AppNavigator = () => (
   <NavigationContainer>
     <Stack.Navigator
-      initialRouteName="Login"
       screenOptions={{
         headerStyle: { backgroundColor: "#5102A1" },
         headerTintColor: "white",
@@ -28,7 +27,7 @@ export const AppNavigator = () => (
         component={Register}
         options={{ headerShown: false }}
       />
-      <Stack.Screen name="Chat" component={Chat} options={{ title: "Chat" }} />
+      <Stack.Screen name="Chat" component={DrawerNavigator} />
     </Stack.Navigator>
   </NavigationContainer>
 );

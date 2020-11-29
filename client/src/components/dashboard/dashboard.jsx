8b87cb19480c72ff83b300cc2e18defc784594a9
thesodbuster <jreichle13@gmail.com>
2020-11-29 16:23:49 -0500

    BottomBar shows up in dialogue window now

--- client/src/components/dashboard/dashboard.jsx
+++ client/src/components/dashboard/dashboard.jsx
@@ -485,6 +485,8 @@ class Dashboard extends Component {
                 </IconButton>
               </Toolbar>
             </AppBar>
+
+            <BottomBar />
           </Dialog>
 
           <Grid item xs={9} style={{height: "100%"}}>
